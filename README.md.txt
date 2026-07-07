71e325dda1c2af9a2fdf41c5b033104e0e4da691
kodelinx <kodelinx.dev@gmail.com>
2026-07-08 02:14:41 +0800

    Move repository root to developer journey folder

---
+++ README.md.txt
@@ -0,0 +1,23 @@
+#Developer Journey
+
+This repository documents my journey to become a Developer in the fuutture
+
+## Goal
+
+to become developer-ready on or before 2027 by relearning and applying the following tools:
+- C#
+-Git and GitHub
+-SQL
+-ASP.NET Core Web API
+-VUE/React
+-Full-stack project development
+
+## Main Project
+
+IT Asset & Support Ticket Management System
+
+## Progress
+
+### Week 0
+
+- Day 1: Set up developer tools for development workflow. Start C# coding.
---
+++ week-0/day-1-csharp-start/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Good Day Future Develoepr! Welcome to your developer journey.");
+
+string name = "KD";
+int currentWeek = 0;
+int currentDay = 1;
+string goal = "achieve my dream of becoming a developer on or before 2027!";
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Date: {currentWeek} + {currentDay}");
+Console.WriteLine($"Goal: {goal}");
+
+string ticketTitle = "Device cannot connect to network";
+string ticketStatus = "Open";
+string assignedTechnician = "KD";
+int ticketNumber = 0;
+int priorityLevel = 3;
+bool isResolved = false;
+
+Console.WriteLine("TICKET");
+Console.WriteLine($"Title: {ticketTitle}");
+Console.WriteLine($"Status: {ticketStatus}");
+Console.WriteLine($"Technician: {assignedTechnician}");
+Console.WriteLine($"Ticket Number: {ticketNumber}");
+Console.WriteLine($"Priority Level: {priorityLevel}");
+Console.WriteLine($"Resolved: {isResolved}");
