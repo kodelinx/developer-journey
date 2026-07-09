d9c304185dbac9ab2da56a1ef0cf426d7c056179
kodelinx <kodelinx.dev@gmail.com>
2026-07-10 00:13:39 +0800

    IASTMS v1: Applied Conditional Statements (if/else if/else) - Input/Output Console | Technician Assignor | Ticket Severity Indicator | Filed Date tracking

--- Project/IASTMS/Program.cs
+++ Project/IASTMS/Program.cs
@@ -17,7 +17,7 @@ Console.Write("Affected Device (Lenovo|MacBook|HP): " );
 string device = Console.ReadLine();
 Console.Write("Ticket Severity (1|2|3): ");
 int severity = Convert.ToInt32(Console.ReadLine());
-Console.Write("Ticket Status (Open|In Progress|Closed0): ");
+Console.Write("Ticket Status (Open|In Progress|Closed): ");
 string status = Console.ReadLine();
 Console.WriteLine("Date of Issue Occurence (Numeric)");
 Console.Write("Month: ");
@@ -38,25 +38,49 @@ Console.WriteLine($"Ticket Subject: {subject}");
 Console.WriteLine($"Ticket Description: {description}");
 Console.WriteLine($"Affected User: {user}");
 Console.WriteLine($"Affected Device: {device}");
-// Decide which technician will takeover this ticket based on the device affected.
+
+// Assign a technician will takeover this ticket based on the device affected.
 if(device == "Lenovo"){
     Console.WriteLine("Technician: Kristian");
-}else if (device == "MacBook"){
+}
+else if (device == "MacBook"){
     Console.WriteLine("Technician: Dave");
-}else{
+}
+else{
     Console.WriteLine("Technician: KD");
 }
+
+// Designate priority level based on indicated severity
+if(severity == 1)
+{
+    Console.WriteLine($"Ticket Priority: High");
+}else if (severity == 2)
+{
+    Console.WriteLine($"Ticket Priority: Medium");
+}else if (severity == 3)
+{
+    Console.WriteLine($"Ticket Priority: Low");
+}
+else{
+    Console.WriteLine($"Ticket Priority: Undefined");
+}
+
 Console.WriteLine($"Ticket Status: {status}");
-Console.WriteLine($"Date of Occurence: {month}/{day}/{year}");
+Console.WriteLine($"Date of Occurence (m/d/y): {month}/{day}/{year}");
 Console.WriteLine();
+
 // Identify ticket status and the corresponding user notification.
-if(status == "Open"){
-    Console.WriteLine("Newly created ticket");
-}else if (status == "In Progress"){
-    Console.WriteLine("Ticket is being handled");
-}else if (status == "Closed"){
-    Console.WriteLine("Ticket is already resolved");
-}else{
+if(status == "Open")
+{
+    Console.WriteLine("A new ticket has been created!");
+}
+else if (status == "In Progress"){
+    Console.WriteLine("Ticket is now being handled");
+}
+else if (status == "Closed"){
+    Console.WriteLine("Ticket has already been resolved");
+}
+else{
     Console.WriteLine("Unknown Status");
 }
 Console.WriteLine();
